bb5cf4a14f4fc0df77b456795fb7e65950baa133
Tommy-83 <tommy.kosakowski@gmail.com>
2023-05-07 16:32:45 +0000

    final commit before i'll try to connect to heroku....

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import sys
 from termcolor import colored
 
 def start_menu():
-    print("Welcome To Wordle by Tommy")
-    print("try and guess the 5 letter word")
+    print("Let's play Wordle:")
+    print("Type a 5 letter word and hit enter!\n")
 
 def pick_a_word():
     with open("words.txt") as f:
@@ -12,22 +12,28 @@ def pick_a_word():
         return random.choice(words)
 
 start_menu()
-word = pick_a_word()
+play_again = ""
+while play_again != "q":
+    
+    word = pick_a_word()
+    for attempt in range(1, 7):
+        guess = input().lower()
 
-for attempt in range(1, 7):
-    guess = input().lower()
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
 
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[1K')
+        for i in range( min(len(guess), 5) ):
+            if guess[i] == word[i]:
+                print(colored(guess[i], 'green'), end="")
+            elif guess[i] in word:
+                print(colored(guess[i], 'yellow'), end="")
+            else:
+                print(guess[i], end="")
+        print()
 
-    for i in range( min(len(guess), 5) ):
-        if guess[i] == word[i]:
-            print(colored(guess[i], 'green'), end="")
-        elif guess[i] in word:
-            print(colored(guess[i], 'yellow'), end="")
-        else:
-            print(guess[i], end="")
-
-    if guess == word:
-            print(colored(f"Well done! You guessed the Wordle in {attempt}", 'green'))
+        if guess == word:
+            print(colored(f"Congrats! You got the wordle in {attempt}", 'green'))
             break
+        elif attempt == 6:
+            print(f"Sorry the wordle was.. {word}",)
+    play_again = input("Want to play again?. Type q to exit.")
