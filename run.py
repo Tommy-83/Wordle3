f2c21607325b00d0827e1af3c82860159311cd6c
Tommy-83 <tommy.kosakowski@gmail.com>
2023-05-07 15:37:46 +0000

    Creating the function to pick a random word from the list

--- run.py
+++ run.py
@@ -4,4 +4,9 @@ def start_menu():
     print("Welcome To Wordle by Tommy")
     print("try and guess the 5 letter word")
 
+def pick_a_word():
+    with open("words.txt") as f:
+        words = f.read().splitlines()
+        return random.choice(words)
+
 start_menu()
